e1653133f10003d53b79c64e7af9bf5913a77ac5
Alireza <52131452+alirezsbashi@users.noreply.github.com>
2020-05-14 06:54:11 -0700

    shape

---
+++ app/src/main/java/arb/test/shape/MainActivity.kt
@@ -0,0 +1,15 @@
+package arb.test.shape
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
